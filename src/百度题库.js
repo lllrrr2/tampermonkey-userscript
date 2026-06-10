6e9c88c0d2289e01101fc5d5440fec1f49fe751d
mryang <yang10560@163.com>
2026-06-11 01:51:59 +0800

    修复bug:显示不全

--- src/百度题库.js
+++ src/百度题库.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         百度题库显示答案
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  百度题库显示答案。
 // @author       夜雨
 // @match        https://easylearn.baidu.com/*
@@ -14,7 +14,12 @@
 (function () {
     'use strict';
 
-    var clearMask = function () {
+    // 设置答案区域字体大小
+    const style = document.createElement('style');
+    style.textContent = '#analysis { font-size: 14px !important; }';
+    document.head.appendChild(style);
+
+    const clearMask = function () {
         if (document.querySelector(".mask")) {
             document.querySelectorAll(".mask").forEach(e => {
                 e.remove();
@@ -25,6 +30,13 @@
                 e.remove();
             })
         }
+        // 移除遮罩class属性（不删除元素）
+        document.querySelectorAll(".analysis-text-mask").forEach(e => {
+            e.classList.remove("analysis-text-mask");
+        })
+        document.querySelectorAll(".shiti-answer").forEach(e => {
+            e.classList.remove("shiti-answer");
+        })
         console.log("清理完毕")
 
     }
